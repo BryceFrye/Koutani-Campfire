ccce37c14811487c054cbf8e70ccb0d71981c2ad
Bryce <bcfrye19@gmail.com>
2011-10-14 21:29:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoutaniCampfire::Application.config.secret_token = 'c4eee31b08923953bfcff6bc8ff2e34cd845a5aad558c4806ca98bade54d970981ead85e05cc74abf94c7ecb04527a49360650680cb5e7d8ae873efade55d95f'
